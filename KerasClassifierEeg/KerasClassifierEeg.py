f91d1f242ccd449cfc133b9ff14455a66f6c2236
Piotr Falkowski <falcon777@wp.pl>
2019-06-13 09:15:02 +0200

    Important improvements - use minMaxNormalizer insted of standard dist based

--- KerasClassifierEeg/KerasClassifierEeg.py
+++ KerasClassifierEeg/KerasClassifierEeg.py
@@ -6,7 +6,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 
 
@@ -28,10 +28,14 @@ class EegBandsClassificationApi:
             self.dataframe = self.dataframe.dropna()    
 
     def __DropUnnededColumns(self):
-        self.dataframe = self.dataframe.drop(["Condition", "TernaryCondition"], axis=1)
+        self.dataframe = self.dataframe.drop(["Condition", "TernaryCondition", "Subject", "Session"], axis=1)
 
     def __EncodeCatData(self):
         self.dataframe['BinaryCondition'] = self.dataframe['BinaryCondition'].map(self.binaryConditionMap)
+
+    def __AssignToX_Y(self):
+        self.X = self.dataframe.drop(['BinaryCondition'], axis=1).values
+        self.Y = self.dataframe["BinaryCondition"].values
 
     def MoldData(self):
         self.__CleanNulls()
@@ -39,25 +43,21 @@ class EegBandsClassificationApi:
         self.__EncodeCatData()
         self.__AssignToX_Y()
 
-    def __AssignToX_Y(self):
-        self.X = self.dataframe.drop(['BinaryCondition'], axis=1).values
-        self.Y = self.dataframe["BinaryCondition"].values
-
     def CreateModel(self):
         if (self.dataframe is None):
             raise ValueError('dataframe is not initialized. Use ReadData() before calling CreateModel()')
         model = Sequential()
         model.add(Dense(self.inputLen(), input_dim = self.inputLen(), kernel_initializer='normal', activation='relu'))
-        model.add(Dense(50, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(30, kernel_initializer='normal', activation='relu'))
         model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))	    
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
 
-    def TrainModel(self, iterations=2000, batchSize=5):
+    def TrainModel(self, iterations=100, batchSize=5):
         if (self.dataframe is None):
             raise ValueError('dataframe is not initialized. Use ReadData() before calling TrainModel()')
         estimators = []
-        estimators.append(('standardize', StandardScaler()))
+        estimators.append(('scale', MinMaxScaler()))
         estimators.append(('mlp', KerasClassifier(build_fn=self.CreateModel, epochs=iterations, batch_size=batchSize, verbose=0)))
         pipeline = Pipeline(estimators)
         kfold = StratifiedKFold(n_splits=10, shuffle=True)
@@ -72,7 +72,7 @@ class EegBandsClassificationApi:
 if __name__ == '__main__':
     # load dataset
     api = EegBandsClassificationApi()
-    api.ReadData("..\AverageBandpowersLabelled_26-5-20-40-48.csv")
+    api.ReadData("..\AvgBandpowers_9-6-13-2_100-15_awake+anesthetized.csv")
     api.MoldData()
     api.TrainModel()
     print(api.GetSummary())
